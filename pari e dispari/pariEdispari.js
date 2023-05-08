e4f8c15c0834f2330cd59349d080e06c3ab9f406
MatteoGunetto <Matteogunetto@tiscali.it>
2023-05-08 16:29:59 +0200

    pari e dispari terminato

--- pari e dispari/pariEdispari.js
+++ pari e dispari/pariEdispari.js
@@ -10,12 +10,10 @@ Dichiariamo chi ha vinto.
 let scelta = prompt('Scommetti scrivendo pari o dispari: ');
 let numUser = parseInt(prompt('Inserisci un numero: '));
 let numComp = numRandom();
+let tot= sumPariDispari();
 
 
-
-// console.log(numUser);
-// console.log(scelta);
-
+// funzioni
 
 function numRandom ()
 {
@@ -25,3 +23,33 @@ function numRandom ()
 
 }
 
+function sumPariDispari(){
+    
+    let somma = (numUser + numComp);
+    
+    if (somma %2 == 0 && scelta == 'pari'){
+        const pari = `La somma è ${ somma}, quindi: PARI e hai vinto`;
+        return pari;
+        
+    }
+    else if (somma %2 == 0 && scelta == 'dipari')
+    {
+        const pari = `La somma è ${ somma}, quindi: PARI e hai perso`;
+        return pari;
+
+    }
+    else if(somma %2 !== 0 && scelta == 'pari'){
+        const dispari = `La somma è ${ somma}, quindi: DISPARI hai perso`;
+        return dispari;
+    }
+    else(somma %2 !== 0 && scelta == 'dispari')
+    {
+        const dispari = `La somma è ${ somma}, quindi: DISPARI hai vinto`;
+        return dispari;
+
+    }
+    
+}
+
+console.log(numUser);
+console.log(tot);
